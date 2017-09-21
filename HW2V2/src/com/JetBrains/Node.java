83a5c01ac59f51df3d003b0de230591dd166bcac
JeffreyCutcliffe <31298370+JeffreyCutcliffe@users.noreply.github.com>
2017-09-20 22:38:56 -0600

    Add files via upload

---
+++ HW2V2/src/com/JetBrains/Node.java
@@ -0,0 +1,19 @@
+package com.JetBrains;
+
+class Node<AnyType>{
+
+    public AnyType data;
+
+    public Node<AnyType> next;
+
+
+
+    public Node(AnyType data, Node<AnyType> next){
+
+        this.data = data;
+
+        this.next = next;
+
+    }
+
+}
